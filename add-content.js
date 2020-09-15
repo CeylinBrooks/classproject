b060df01c73fd58892a960e113a6cb8e4bdeeb82
ceylinbrooks <ceylinbrooks@gmail.com>
2020-09-15 17:09:48 -0400

    prompt

--- add-content.js
+++ add-content.js
@@ -1,3 +1,5 @@
+'use strict'
+
 function askFlower() {
     var userName = prompt('What flower do you like?');
     return document.write('<h3> You\'re right  ' + userName + ' are nice </h3>');
@@ -5,4 +7,16 @@ function askFlower() {
   
   askFlower();
 
+  function askColor() {
+    var userName = prompt('Do you want to plant a garden today or tommorow?');
+    return document.write('<h3> Alright then, let\'s get started on ' + userName + ' </h3>');
+  }
   
+  askFlower();
+
+  function ask() {
+    var userName = prompt('What flower do you like?');
+    return document.write('<h3> You\'re right  ' + userName + ' are nice </h3>');
+  }
+  
+  askFlower();
