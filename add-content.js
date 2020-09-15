144b5a5b3f52bbbda95fabc2840b9c653aa263be
ceylinbrooks <ceylinbrooks@gmail.com>
2020-09-15 17:23:08 -0400

    content js

--- add-content.js
+++ add-content.js
@@ -1,22 +1,22 @@
 'use strict'
 
 function askFlower() {
-    var userName = prompt('What flower do you like?');
-    return document.write('<h3> You\'re right  ' + userName + ' are nice </h3>');
+    var askFlower = prompt('What flower do you like?');
+    return document.write('<h3> You\'re right  ' + askFlower + ' are nice </h3>');
   }
   
   askFlower();
 
   function askColor() {
-    var userName = prompt('Do you want to plant a garden today or tommorow?');
-    return document.write('<h3> Alright then, let\'s get started on ' + userName + ' </h3>');
+    var askColor = prompt('Do you want to plant a garden today or tommorow?');
+    return document.write('<h3> Alright then, let\'s get started on ' + askColor + ' </h3>');
   }
   
-  askFlower();
+  askColor();
 
-  function ask() {
-    var userName = prompt('What flower do you like?');
-    return document.write('<h3> You\'re right  ' + userName + ' are nice </h3>');
+  function askYard() {
+    var askYard = prompt('Do you have a large or small space?');
+    return document.write('<h3> Okay, we can work that ' + askYard + ' </h3>');
   }
   
-  askFlower();
+  askYard();
