566a167f8cd21770618eb43a05c8d2f342ba364f
ceylinbrooks <ceylinbrooks@gmail.com>
2020-09-15 23:47:52 -0400

    added get count

--- add-content.js
+++ add-content.js
@@ -2,22 +2,32 @@
 
 function askFlower() {
     var askFlower = prompt('What flower do you like?');
-    return document.write('<h3> You\'re right  ' + askFlower + ' are nice </h3>');
+    return document.write('<h3> You\'re right  ' + askFlower + ' are wonderful </h3>');
   }
-    
   
+
+
   askFlower();
 
   function askColor() {
     var askColor = prompt('Do you want to plant a garden today or tommorow?');
-    return document.write('<h3> Alright then, let\'s get started on ' + askColor + ' </h3>');
+    return document.write('<h3> Alright then, let\'s get started on that ' + askColor + ' </h3>');
   }
   
   askColor();
 
   function askYard() {
     var askYard = prompt('Do you have a large or small space?');
-    return document.write('<h3> Okay, we can work that ' + askYard + ' </h3>');
+    return document.write('<h3> Okay, we can work that ' + askYard + ' garden </h3>');
   }
   
   askYard();
+
+  function getCount(){
+    var count = prompt ('Great! How many would you like?')
+    while(isNaN(count)) {
+      count = prompt("PLEASE enter a number. How many would you like?");
+    }
+    return count;
+  }
+getCount();
